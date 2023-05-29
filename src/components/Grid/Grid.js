11994a6e873f0b883c7a4246ccabaa2122347e1c
marlenedFR <d.marlene@gmail.com>
2023-05-29 15:31:21 +0200

    results appears only when search has been done

--- src/components/Grid/Grid.js
+++ src/components/Grid/Grid.js
@@ -1,40 +1,43 @@
 import React, { useState } from "react";
 
 import City from "../City/City";
-import Title from "../Title/Title";
 import Weather from "../Weather/Weather";
 import SearchBar from "../SearchBar/SearchBar";
 
 import "./Grid.css";
-import Logo from "../Logo/Logo";
 import { fetchCityPhoto } from "../../api/unsplash";
+import Header from "../Header/Header";
 
 function Grid() {
   const [cities, setCityWeather] = useState([]);
   const [cityPhoto, setCityPhoto] = useState(null);
+  const [hasSearched, setHasSearched] = useState(false); // Etat pour suivre si une recherche a été effectuée
 
   const handleSearch = async (filteredCities) => {
     setCityWeather(filteredCities);
 
     if (filteredCities.length > 0) {
-      const photo = await fetchCityPhoto(filteredCities[0].name); // Remplacez "filteredCities[0].name" par la propriété correspondant au nom de la ville dans le tableau filteredCities
+      const photo = await fetchCityPhoto(filteredCities[0].name);
       setCityPhoto(photo);
     }
+    setHasSearched(true);
+
   };
 
   return (
     <div className="grid">
       <div className="top-row">
-        <Title />
-        <Logo />
+        <Header />
       </div>
       <div className="middle-row">
         <SearchBar onSearch={handleSearch} />
       </div>
-      <div className="bottom-row">
-        <City cityPhoto={cityPhoto} />
-        <Weather cities={cities} />      
-      </div>
+      {hasSearched ? (
+        <div className={`bottom-row ${hasSearched ? "slide-in" : ""}`}>
+          <City cityPhoto={cityPhoto} />
+          <Weather cities={cities} />      
+        </div>
+      ) : null}
     </div>
   );
 }
