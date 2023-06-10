5dad93fafce1563bdec6fe5e73897f2d4018801e
marlenedFR <d.marlene@gmail.com>
2023-06-10 10:49:28 +0200

    change style

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -6,14 +6,16 @@ function Sidebar({ onClose, showSidebar }) {
 
 
   return (
-    <div className={`sidebar ${showSidebar ? "open" : ""}`}>
-      <i className="close icon" onClick={onClose}></i>
-      <h3>À propos</h3>
-      <p>Meteo Flash utilise des bases de données gratuites, par conséquent, les recherches sont limitées et les résultats peuvent ne pas refléter la réalité.</p>
-      <p>Les suggestions affichent les villes les plus populaires avec une densité de population de 15000 habitants minimum. </p>
-      <p>Les photos proviennent du site <a href="https://unsplash.com/" target="_blank" rel="noreferrer">Unsplash</a> et peuvent ne pas être pertinentes par rapport aux recherches effectuées.</p>
-      <p>Le nombre de requêtes étant limitées, le site peut ne pas fonctionner correctement à certains moments.</p>
-      <p>Merci de votre compréhension.</p>
+    <div className="sidebar-container">
+      <div className={`sidebar ${showSidebar ? "open" : ""}`}>
+        <i className="close icon" onClick={onClose}></i>
+        <h3>À propos</h3>
+        <p>Meteo Flash utilise des bases de données gratuites, par conséquent, les recherches sont limitées et les résultats peuvent ne pas refléter la réalité.</p>
+        <p>Les suggestions affichent les villes les plus populaires avec une densité de population de 15000 habitants minimum. </p>
+        <p>Les photos proviennent du site <a href="https://unsplash.com/" target="_blank" rel="noreferrer">Unsplash</a> et peuvent ne pas être pertinentes par rapport aux recherches effectuées.</p>
+        <p>Le nombre de requêtes étant limitées, le site peut ne pas fonctionner correctement à certains moments.</p>
+        <p>Merci de votre compréhension.</p>
+      </div>
     </div>
 
   );
