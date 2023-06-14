ff845f96304ff06bed515988df7585838dbd7d8b
marlenedFR <d.marlene@gmail.com>
2023-06-14 16:51:08 +0200

    add analytics

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,19 @@
 import React from "react";
+import { Analytics } from "@vercel/analytics/react";
 
 import "semantic-ui-css/semantic.min.css";
 import "./App.css";
 
 import Grid from "../Grid/Grid";
 
-function App() {
+// eslint-disable-next-line react/prop-types
+function App({ Component, pageProps }) {
   return (
     <div className="App">
+      <>
+        <Component {...pageProps} />
+        <Analytics />
+      </>
       <Grid />
     </div>
   );
