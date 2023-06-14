da427d5efe4516e8a8e881674f5ed775b34ac2ad
marlenedFR <d.marlene@gmail.com>
2023-06-14 16:27:37 +0200

    add scroll after research

--- src/components/Grid/Grid.js
+++ src/components/Grid/Grid.js
@@ -47,6 +47,7 @@ function Grid() {
       setCityPhoto(null); // If no filtered cities, reset the photo state
     }
     setShowCityContainer(true); // Make the city container visible after a search operation
+    window.scrollTo({ top: 0, behavior: "smooth" }); // Faites défiler la vue vers le haut après la recherche    
   };
 
   return (
