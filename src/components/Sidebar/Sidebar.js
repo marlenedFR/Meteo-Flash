7763ffc67d142f130a697b35a4bff412d0482f84
marlenedFR <d.marlene@gmail.com>
2023-06-15 10:30:15 +0200

    fix sidebar text

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -9,9 +9,9 @@ function Sidebar({ onClose, showSidebar, weatherIcon }) {
       <div className={`sidebar ${showSidebar ? "open" : ""}`}>
         <i className="close icon" onClick={onClose}></i>
         <h3>À propos</h3>
-        <p>Meteo Flash s&#39;appuie sur des bases de données gratuites, par conséquent, les recherches sont limitées et les résultats peuvent ne pas refléter la réalité.</p>
-        <p>Les suggestions de recherche privilégient les villes les plus populaires ayant une densité de population d&#39;au moins 15 000 habitants.</p>
-        <p>Les images affichées sont sélectionnées en fonction de leur pertinence générale et peuvent ne pas correspondre exactement à la ville recherchée.</p>
+        <p>Meteo Flash s&#39;appuie sur des bases de données gratuites, ce qui implique certaines limites.</p>
+        <p>Les suggestions de recherche privilégient les villes les plus populaires ayant une densité de population d&#39;au moins 15 000 habitants, afin de proposer des résultats pertinents.</p>
+        <p>Les images affichées sont sélectionnées en fonction de leur pertinence générale mais peuvent ne pas correspondre exactement à la ville recherchée.</p>
         <p>En raison de la limitation du nombre de requêtes que nous pouvons faire aux sources de données gratuites, le site peut parfois ne pas fonctionner comme prévu. Si vous rencontrez des problèmes, je vous invite à réessayer ultérieurement.</p>
         <p>Merci de votre compréhension.</p>
       </div>
